be50e8b789bff0281ca27fc5dc83de1ad2dd0a2c
ICarlos-s18 <csanchezhernandez567@gmail.com>
2023-06-09 16:26:25 -0600

    02 variables

---
+++ main.py
@@ -0,0 +1,9 @@
+# Fuction print
+print("Hola Mundo")
+print("soy ese")
+
+print("Podemos evitar Diferentes Tipos de Datos")
+print(12)
+print(12.23)
+print(True)
+print([])
---
+++ variables.py
@@ -0,0 +1,14 @@
+# Pyton es No tipado
+name ="Carlos"
+last_name ="Alberto"
+age = 18
+height = 1.67
+is_student = True 
+groups = ["3C"] 
+
+# fuction type
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+print(type(groups))
