a51b216df3331bd952a3d6c57bc0b30501df3da0
ICarlos-s18 <csanchezhernandez567@gmail.com>
2023-06-09 16:44:24 -0600

    02 variables

---
+++ entrada.py
@@ -0,0 +1,19 @@
+# fuction input -> retorna string
+
+num_a = input("Ingresa un numero: ")
+num_a = input("Ingresa un numero: ")
+
+print(num_a + num_b)
+
+name = input("Ingresa tu nombre: ")
+age = int(input("Ingresa tu edad: "))
+city = input("Ingresa tu ciudad/pueblo")
+
+# string format
+"""
+  Holaa, soy name, tengo age años y vivo en city
+"""
+greeting = "Holaa, soy {}, tengo {} años y vivo en {}"
+greeting.format(name, age, city)
+
+print(greeting)
