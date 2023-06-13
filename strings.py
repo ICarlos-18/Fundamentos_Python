86883e7aa01e5a86a4f5a2b42aaabec472bfd292
ICarlos-s18 <csanchezhernandez567@gmail.com>
2023-06-13 13:01:02 -0600

    07 Strings

---
+++ strings.py
@@ -0,0 +1,24 @@
+my_string = "esTe eS Mi StrInG "
+
+# longitud de string
+print(len(my_string))
+# Mayúsculas
+print(my_string.upper())
+# Minísculas
+print(my_string.lower())
+# Capitalice primera letra a Mayus
+print(my_string.capitalize())
+# Eliminar espacios incio fin
+print(my_string.strip())
+
+# Indexing and Slicing
+my_string = "hola 3C DSM"
+print(my_string[0])
+print(my_string[6])
+print(my_string[3])
+print(my_string[-1])
+print(my_string[-2])
+
+print(my_string[0:2])
+print(my_string[5:7])
+print(my_string[5:-1])
